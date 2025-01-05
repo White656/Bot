7a68f1a765cc832503fd65e72f250151929266fc
kirill.khorkov <k.gold@blinkmap.com>
2025-01-05 23:16:51 +0300

    Add relationship between Docs and MilvusDocs models
    Introduced a bidirectional relationship between the `Docs` and `MilvusDocs` models using SQLAlchemy ORM. Updated the `Docs` model with a `milvus_docs` attribute and the `MilvusDocs` model with a `docs` attribute. Also added a new migration script to handle this database change.

--- internal/entity/docs.py
+++ internal/entity/docs.py
@@ -3,15 +3,18 @@ from sqlalchemy.dialects import postgresql as psql
 
 from internal.entity.base import Base
 from internal.entity.mixin import TimestampMixin
+from sqlalchemy.orm import relationship
 
 
 class Docs(TimestampMixin, Base):
     name = sa.Column(sa.String(255), nullable=True)
     checksum = sa.Column(sa.BigInteger, nullable=True)
     s3_briefly = sa.Column(sa.String(255), nullable=True)
+    milvus_docs = relationship('MilvusDocs', back_populates='docs', uselist=True)
 
 
 class MilvusDocs(TimestampMixin, Base):
     id = ...
     milvus_id = sa.Column(sa.Integer, primary_key=True)
     docs_id = sa.Column(psql.UUID(as_uuid=True), sa.ForeignKey('docs.id'), primary_key=True, nullable=False)
+    docs = relationship('Docs', back_populates='milvus_docs')
