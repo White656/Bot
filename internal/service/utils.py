1f2d278dc55e17e9724e7f3f74ca0571c27d8604
kirill.khorkov <k.gold@blinkmap.com>
2025-01-05 23:09:52 +0300

    Refactor `get_service` to a dedicated utility module.
    Moved the `get_service` async context manager from `celery/worker.py` to a new `internal/service/utils.py` module for better modularity and reusability. This change improves code organization and isolates utility functions from worker-specific logic.

---
+++ internal/service/utils.py
@@ -0,0 +1,32 @@
+from contextlib import asynccontextmanager
+
+from internal.config.database import get_database_client
+
+
+@asynccontextmanager
+async def get_service(service_class):
+    """
+    An asynchronous context manager for obtaining and managing a service instance.
+
+    This function is designed to provide a service instance of the specified
+    service class, utilizing a session created by the asynchronous database client.
+    It ensures proper cleanup of the session once the context is exited. This
+    function supports dependency injection by creating the service with the session
+    as its initialization parameter.
+
+    Args:
+        service_class: The class of the service to be managed. This should be a
+        subclass of `Service`.
+
+    Yields:
+        An instance of the provided `service_class`, initialized with the session.
+
+    Raises:
+        Any exceptions raised within the context will propagate back to the caller.
+    """
+    async for session in get_database_client():
+        service = service_class(session)
+        try:
+            yield service
+        finally:
+            await session.close()
